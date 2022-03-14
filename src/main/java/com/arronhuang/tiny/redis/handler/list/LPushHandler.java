d4bc8094398db7dd9fb02c31ecc50bcdd2155770
arronhuang <har025@qq.com>
2022-03-14 20:32:09 +0800

    新增 lpush 的单元测试

--- src/main/java/com/arronhuang/tiny/redis/handler/list/LPushHandler.java
+++ src/main/java/com/arronhuang/tiny/redis/handler/list/LPushHandler.java
@@ -1,5 +1,28 @@
 package com.arronhuang.tiny.redis.handler.list;
 
-public class LPushHandler {
+import com.arronhuang.tiny.redis.netty.RespResponse;
+import com.arronhuang.tiny.redis.storage.RedisList;
+
+import java.util.List;
+
+public class LPushHandler extends AbstractListCommandHandler {
+
+    @Override
+    public RespResponse doHandle(List<String> args) {
+        String key = args.get(0);
+        RedisList redisList = get(key);
+
+        for (int i = 1; i < args.size(); i++) {
+            String element = args.get(i);
+            redisList.add(0, element);
+        }
+
+        return RespResponse.number(redisList.size());
+    }
+
+    @Override
+    public void checkArgs(List<String> args) {
+
+    }
 
 }
--- src/main/java/com/arronhuang/tiny/redis/storage/RedisList.java
+++ src/main/java/com/arronhuang/tiny/redis/storage/RedisList.java
@@ -1,12 +1,49 @@
 package com.arronhuang.tiny.redis.storage;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
 import lombok.Data;
 
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.List;
 
 @Data
 public class RedisList extends RedisObject {
 
     private List<String> value;
+
+    private RedisList(Collection<String> elements) {
+        this.value = new LinkedList<>(elements);
+    }
+
+    public void add(int index, String element) {
+        if (value == null) {
+            value = new LinkedList<>();
+        }
+
+        value.add(index, element);
+    }
+
+    public int findFirstElement(String element) {
+        if (CollUtil.isEmpty(value)) {
+            return -1;
+        }
+
+        for (int i = 0; i < value.size(); i++) {
+            if (StrUtil.equals(value.get(i), element)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public int size() {
+        if (CollUtil.isEmpty(value)) {
+            return 0;
+        }
+        return value.size();
+    }
 
 }
